f033e132b82b2604c86da24fe31d158129008f72
Colin Arpe <colin.j.arpe@outlook.com>
2016-12-06 15:26:25 -0500

    Disable selected letters, place letters in game board, format layout of game board

--- Assets/Scripts/scripts.js
+++ Assets/Scripts/scripts.js
@@ -2,20 +2,26 @@ $(document).ready(function() {
 // Fill the array with the letters of the string
 	for (var i = 0; i < answer.length; i++) {
 		if (answer[i] !== " ")	{
-			answerArray.push (answer[i].toUpperCase);
+			answerArray.push (answer[i].toUpperCase());
 			thisWord++;
 		}	else	{
 			words.push (thisWord);
 			thisWord = 1;
 		}
 	}
+	words.push (thisWord);
 
 // Create the game board
+	var k = 0;
 	for (var i = 0; i < words.length; i++)	{
-		for (var j = 0; j < words[i]; j++)	{
-			$("#game-board").append("<p class='letter-box'></p>");
+		for (var j = 0; j < words[i]-1; j++)	{
+			$("#game-board").append(
+				"<div class='letter-box'>" +
+					"<p class='letter'>" + answerArray[k] + "</p>" +
+				"</div>");
+			k++;
 		}
-		$("#game-board").append("<p class='space-box'></p>");
+		$("#game-board").append("<br>");
 	}
 
 
@@ -23,9 +29,9 @@ $(document).ready(function() {
 	for (var i = 65; i <= 90; i++) {
 		$("#alpha-list").append("<option class='letter' value='" + String.fromCharCode(i) + "'>" + String.fromCharCode(i) + "</option>");
 	}
-	$("#alpha-list").select (function()	{
-		guess = $("#alpha-list").value;
-console.log(guess);
+	$("#alpha-list").change (function()	{
+		guess = $("#alpha-list").val();
+		$(".letter").eq(guess.charCodeAt()-65).attr("disabled", "true");
 	});
 
 });
